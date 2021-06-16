a7fc5517eef2c9e08fced58aeed08493d95a8388
Vincent den Boer <vincent@youapt.eu>
2021-06-16 11:47:31 +0200

    Update Firebase imports

--- ts/firebase.ts
+++ ts/firebase.ts
@@ -1,35 +1,35 @@
 import { EventEmitter } from 'events';
-import * as firebase from 'firebase'
+import firebase from 'firebase/app'
 import { SignalTransport, SignalChannel, SignalMessageOptions, SignalChannelEvents, SignalDeviceId } from './types';
 import { getReceiverDeviceId } from './utils';
 
 type FirebaseSignalMessage = string
 interface FirebaseSignalChannelData {
-    created : typeof firebase.database.ServerValue.TIMESTAMP,
-    firstQueue? : FirebaseSignalMessage[] // cannot overwrite messages, just push
-    secondQueue? : FirebaseSignalMessage[]
+    created: typeof firebase.database.ServerValue.TIMESTAMP,
+    firstQueue?: FirebaseSignalMessage[] // cannot overwrite messages, just push
+    secondQueue?: FirebaseSignalMessage[]
 }
 
 export class FirebaseSignalTransport implements SignalTransport {
-    constructor(private options : {
-        database : firebase.database.Database, collectionName : string,
+    constructor(private options: {
+        database: firebase.database.Database, collectionName: string,
     }) {
     }
 
-    async allocateChannel() : Promise<{ initialMessage : string }> {
-        const channelData : FirebaseSignalChannelData = {
+    async allocateChannel(): Promise<{ initialMessage: string }> {
+        const channelData: FirebaseSignalChannelData = {
             created: firebase.database.ServerValue.TIMESTAMP,
         }
         const ref = this.options.database.ref(this.options.collectionName).push(channelData)
         return { initialMessage: [ref.key].join(':') }
     }
 
-    async openChannel(options : { deviceId : SignalDeviceId, initialMessage? : string }) : Promise<SignalChannel> {
+    async openChannel(options: { deviceId: SignalDeviceId, initialMessage?: string }): Promise<SignalChannel> {
         if (!options.initialMessage) {
             throw new Error(`Opening a channel without an initial message is not supported yet`)
         }
 
-        const [ key ] = options.initialMessage.split(':')
+        const [key] = options.initialMessage.split(':')
         const channelRef = this.options.database.ref(this.options.collectionName).child(key)
         await channelRef.child('created').set(firebase.database.ServerValue.TIMESTAMP)
         return new FirebaseSignalChannel({ channelRef, deviceId: options.deviceId })
@@ -38,9 +38,9 @@ export class FirebaseSignalTransport implements SignalTransport {
 
 export class FirebaseSignalChannel implements SignalChannel {
     events = new EventEmitter() as SignalChannelEvents
-    private receiverQueueRef : firebase.database.Reference
+    private receiverQueueRef: firebase.database.Reference
 
-    constructor(private options : { channelRef : firebase.database.Reference, deviceId : SignalDeviceId }) {
+    constructor(private options: { channelRef: firebase.database.Reference, deviceId: SignalDeviceId }) {
         this.receiverQueueRef = options.channelRef.child(`${getReceiverDeviceId(options.deviceId)}Queue`)
     }
 
@@ -48,12 +48,12 @@ export class FirebaseSignalChannel implements SignalChannel {
         this.options.channelRef.child(`${this.options.deviceId}Queue`).on('child_added', this._processMessage)
     }
 
-    async sendMessage(payload : string, options : SignalMessageOptions) : Promise<void> {
+    async sendMessage(payload: string, options: SignalMessageOptions): Promise<void> {
         // await this.options.channelRef.child('created').set(firebase.database.ServerValue.TIMESTAMP)
         await this._pushToReceiverQueue(payload)
     }
 
-    async _pushToReceiverQueue(message : FirebaseSignalMessage) {
+    async _pushToReceiverQueue(message: FirebaseSignalMessage) {
         await this.receiverQueueRef.push(message)
     }
 
@@ -62,8 +62,8 @@ export class FirebaseSignalChannel implements SignalChannel {
         await this.options.channelRef.remove()
     }
 
-    _processMessage = (snapshot : firebase.database.DataSnapshot) => {
-        const message : FirebaseSignalMessage = snapshot.val()
+    _processMessage = (snapshot: firebase.database.DataSnapshot) => {
+        const message: FirebaseSignalMessage = snapshot.val()
         this.events.emit('signal', { payload: message })
 
     }
