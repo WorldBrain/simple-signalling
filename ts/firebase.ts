11d5694c83cef975c6872c954c6916bd63edf15c
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2022-08-17 15:30:30 +0700

    Update for Firebase SDK v9

--- ts/firebase.ts
+++ ts/firebase.ts
@@ -1,38 +1,60 @@
-import { EventEmitter } from 'events';
-import firebase from 'firebase/app'
-import { SignalTransport, SignalChannel, SignalMessageOptions, SignalChannelEvents, SignalDeviceId } from './types';
-import { getReceiverDeviceId } from './utils';
+import { EventEmitter } from 'events'
+import firebase from 'firebase/compat/app'
+import {
+    SignalTransport,
+    SignalChannel,
+    SignalMessageOptions,
+    SignalChannelEvents,
+    SignalDeviceId,
+} from './types'
+import { getReceiverDeviceId } from './utils'
 
 type FirebaseSignalMessage = string
 interface FirebaseSignalChannelData {
-    created: typeof firebase.database.ServerValue.TIMESTAMP,
+    created: typeof firebase.database.ServerValue.TIMESTAMP
     firstQueue?: FirebaseSignalMessage[] // cannot overwrite messages, just push
     secondQueue?: FirebaseSignalMessage[]
 }
 
 export class FirebaseSignalTransport implements SignalTransport {
-    constructor(private options: {
-        database: firebase.database.Database, collectionName: string,
-    }) {
-    }
+    constructor(
+        private options: {
+            database: firebase.database.Database
+            collectionName: string
+        },
+    ) {}
 
     async allocateChannel(): Promise<{ initialMessage: string }> {
         const channelData: FirebaseSignalChannelData = {
             created: firebase.database.ServerValue.TIMESTAMP,
         }
-        const ref = this.options.database.ref(this.options.collectionName).push(channelData)
+        const ref = this.options.database
+            .ref(this.options.collectionName)
+            .push(channelData)
         return { initialMessage: [ref.key].join(':') }
     }
 
-    async openChannel(options: { deviceId: SignalDeviceId, initialMessage?: string }): Promise<SignalChannel> {
+    async openChannel(options: {
+        deviceId: SignalDeviceId
+        initialMessage?: string
+    }): Promise<SignalChannel> {
         if (!options.initialMessage) {
-            throw new Error(`Opening a channel without an initial message is not supported yet`)
+            throw new Error(
+                `Opening a channel without an initial message is not supported yet`,
+            )
         }
 
         const [key] = options.initialMessage.split(':')
-        const channelRef = this.options.database.ref(this.options.collectionName).child(key)
-        await channelRef.child('created').set(firebase.database.ServerValue.TIMESTAMP)
-        return new FirebaseSignalChannel({ channelRef, deviceId: options.deviceId })
+        const channelRef = this.options.database
+            .ref(this.options.collectionName)
+            .child(key)
+        await channelRef
+            .child('created')
+            .set(firebase.database.ServerValue.TIMESTAMP)
+        return new FirebaseSignalChannel({
+            channelRef,
+            deviceId: options.deviceId,
+        })
     }
 }
 
@@ -40,15 +62,27 @@ export class FirebaseSignalChannel implements SignalChannel {
     events = new EventEmitter() as SignalChannelEvents
     private receiverQueueRef: firebase.database.Reference
 
-    constructor(private options: { channelRef: firebase.database.Reference, deviceId: SignalDeviceId }) {
-        this.receiverQueueRef = options.channelRef.child(`${getReceiverDeviceId(options.deviceId)}Queue`)
+    constructor(
+        private options: {
+            channelRef: firebase.database.Reference
+            deviceId: SignalDeviceId
+        },
+    ) {
+        this.receiverQueueRef = options.channelRef.child(
+            `${getReceiverDeviceId(options.deviceId)}Queue`,
+        )
     }
 
     async connect() {
-        this.options.channelRef.child(`${this.options.deviceId}Queue`).on('child_added', this._processMessage)
+        this.options.channelRef
+            .child(`${this.options.deviceId}Queue`)
+            .on('child_added', this._processMessage)
     }
 
-    async sendMessage(payload: string, options: SignalMessageOptions): Promise<void> {
+    async sendMessage(
+        payload: string,
+        options: SignalMessageOptions,
+    ): Promise<void> {
         // await this.options.channelRef.child('created').set(firebase.database.ServerValue.TIMESTAMP)
         await this._pushToReceiverQueue(payload)
     }
@@ -65,28 +99,29 @@ export class FirebaseSignalChannel implements SignalChannel {
     _processMessage = (snapshot: firebase.database.DataSnapshot) => {
         const message: FirebaseSignalMessage = snapshot.val()
         this.events.emit('signal', { payload: message })
-
     }
 }
 
 export function getSignallingRules() {
     const queueRules = {
-        "$message": {
-            ".validate": "data.val() == null && newData.isString() && newData.val().length < 10240"
+        $message: {
+            '.validate':
+                'data.val() == null && newData.isString() && newData.val().length < 10240',
         },
     }
 
     return {
-        "$id": {
-            ".read": true,
-            ".write": true,
-            ".indexOn": ["created"],
-            ".validate": "data.child('created').val() != null || newData.child('created').val() == now",
+        $id: {
+            '.read': true,
+            '.write': true,
+            '.indexOn': ['created'],
+            '.validate':
+                "data.child('created').val() != null || newData.child('created').val() == now",
 
-            "firstQueue": queueRules,
-            "secondQueue": queueRules,
+            firstQueue: queueRules,
+            secondQueue: queueRules,
 
             // ".validate": "newData.child('payload').isString() && newData.child('deviceId').isString() && newData.child('type').val().matches(/^initial|message|confirmation$/) && newData.child('confirm').isBoolean() && newData.child('created').val() === now",
-        }
+        },
     }
 }
--- ts/simple-peer.test.ts
+++ ts/simple-peer.test.ts
@@ -1,33 +1,31 @@
 import expect from 'expect'
 const wrtc = require('wrtc')
 import Peer from 'simple-peer'
-import * as firebase from 'firebase'
-import { MemorySignalTransportManager } from './memory';
-import { signalSimplePeer } from './simple-peer';
-import { SignalTransport } from './types';
-import { createSignallingFirebaseTestApp } from './firebase.tests';
-import { FirebaseSignalTransport } from './firebase';
+import { MemorySignalTransportManager } from './memory'
+import { signalSimplePeer } from './simple-peer'
+import { SignalTransport } from './types'
+import { createSignallingFirebaseTestApp } from './firebase.tests'
+import { FirebaseSignalTransport } from './firebase'
 
-async function runSimplePeerTest(options : { transports : [SignalTransport, SignalTransport] }) {
+async function runSimplePeerTest(options: {
+    transports: [SignalTransport, SignalTransport]
+}) {
     const { transports } = options
     const { initialMessage } = await transports[0].allocateChannel()
     const channels = [
         await transports[0].openChannel({ initialMessage, deviceId: 'first' }),
         await transports[1].openChannel({ initialMessage, deviceId: 'second' }),
     ]
-    await Promise.all(channels.map(channel => channel.connect()))
+    await Promise.all(channels.map((channel) => channel.connect()))
 
-    const peers = [
-        new Peer({ initiator: true, wrtc }),
-        new Peer({ wrtc }),
-    ]
+    const peers = [new Peer({ initiator: true, wrtc }), new Peer({ wrtc })]
     await Promise.all([
         signalSimplePeer({ signalChannel: channels[0], simplePeer: peers[0] }),
         signalSimplePeer({ signalChannel: channels[1], simplePeer: peers[1] }),
     ])
-    
-    const peerOneData = new Promise<any>(resolve => {
-        peers[0].once('data', data => {
+
+    const peerOneData = new Promise<any>((resolve) => {
+        peers[0].once('data', (data) => {
             resolve(data)
         })
     })
@@ -39,15 +37,25 @@ if (process.env.RUN_FIREBASE_TESTS === 'true') {
     describe('simple-peer connection initiation', () => {
         it('should be able to establish a simple-peer WebRTC connection using an in-memory signal channel', async () => {
             const transportManager = new MemorySignalTransportManager()
-            const transports : [SignalTransport, SignalTransport] = [transportManager.createTransport(), transportManager.createTransport()]
+            const transports: [SignalTransport, SignalTransport] = [
+                transportManager.createTransport(),
+                transportManager.createTransport(),
+            ]
             await runSimplePeerTest({ transports })
         })
-        
+
         it('should be able to establish a simple-peer WebRTC connection using an emulated Firebase channel', async () => {
-            const { app: firebaseApp, collectionName } = await createSignallingFirebaseTestApp()
+            const {
+                app: firebaseApp,
+                collectionName,
+            } = await createSignallingFirebaseTestApp()
             try {
-                const createTransport = () => new FirebaseSignalTransport({ database: firebaseApp.database(), collectionName })
-                const transports : [SignalTransport, SignalTransport] = [
+                const createTransport = () =>
+                    new FirebaseSignalTransport({
+                        database: firebaseApp.database(),
+                        collectionName,
+                    })
+                const transports: [SignalTransport, SignalTransport] = [
                     createTransport(),
                     createTransport(),
                 ]
@@ -72,6 +80,6 @@ if (process.env.RUN_FIREBASE_TESTS === 'true') {
         //         createTransport(),
         //     ]
         //     await runSimplePeerTest({ transports })
-    // })
+        // })
     })
 }
